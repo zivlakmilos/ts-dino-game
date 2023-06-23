587220296a6ca623ce5390274f442b11ee9d289c
Milos Zivlak <zivlakmilos@gmail.com>
2023-06-23 04:26:36 +0200

    Roll out ground

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -3,6 +3,7 @@ import Player from "../entities/Player";
 
 class PlayScene extends Phaser.Scene {
   player: Player;
+  ground: Phaser.GameObjects.TileSprite;
   startTrigger: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
 
   constructor() {
@@ -11,6 +12,10 @@ class PlayScene extends Phaser.Scene {
 
   get gameHeight() {
     return this.game.config.height as number;
+  }
+
+  get gameWidth() {
+    return this.game.config.width as number;
   }
 
   create() {
@@ -22,11 +27,27 @@ class PlayScene extends Phaser.Scene {
       .setOrigin(0, 1);
 
     this.physics.add.overlap(this.startTrigger, this.player, () => {
+      if (this.startTrigger.y === 10) {
+        this.startTrigger.body.reset(0, this.gameHeight);
+        return;
+      }
+
+      this.startTrigger.body.reset(9999, 9999);
+
+      this.time.addEvent({
+        delay: 1000 / 60,
+        loop: true,
+        callback: () => {
+          if (this.ground.width <= this.gameWidth) {
+            this.ground.width += 17 * 2;
+          }
+        }
+      });
     });
   }
 
   createEnvironment() {
-    this.add
+    this.ground = this.add
       .tileSprite(0, this.gameHeight, 88, 26, 'ground')
       .setOrigin(0, 1);
   }
